304e4cbf3395a6ff7215f5e04a956e8166714e16
Fernando Catuaba <fernandocatuaba1@gmail.com>
2022-04-25 16:15:14 -0300

    W.I.P. Montagem do formulário.

---
+++ HandRanges/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace HandRanges
+{
+    partial class HandRanges
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Painel = new System.Windows.Forms.TabControl();
+            this.guiaPrincipal = new System.Windows.Forms.TabPage();
+            this.lbBTN = new System.Windows.Forms.Label();
+            this.lbSB = new System.Windows.Forms.Label();
+            this.lbBB = new System.Windows.Forms.Label();
+            this.lbUTG = new System.Windows.Forms.Label();
+            this.lbUTG1 = new System.Windows.Forms.Label();
+            this.lbMP1 = new System.Windows.Forms.Label();
+            this.lbMP2 = new System.Windows.Forms.Label();
+            this.lbHJ = new System.Windows.Forms.Label();
+            this.lbCO = new System.Windows.Forms.Label();
+            this.Painel.SuspendLayout();
+            this.guiaPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Painel
+            // 
+            this.Painel.Controls.Add(this.guiaPrincipal);
+            this.Painel.Location = new System.Drawing.Point(-7, -29);
+            this.Painel.Name = "Painel";
+            this.Painel.SelectedIndex = 0;
+            this.Painel.Size = new System.Drawing.Size(816, 486);
+            this.Painel.TabIndex = 0;
+            // 
+            // guiaPrincipal
+            // 
+            this.guiaPrincipal.BackgroundImage = global::HandRanges.Properties.Resources.mesa;
+            this.guiaPrincipal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.guiaPrincipal.Controls.Add(this.lbCO);
+            this.guiaPrincipal.Controls.Add(this.lbHJ);
+            this.guiaPrincipal.Controls.Add(this.lbMP2);
+            this.guiaPrincipal.Controls.Add(this.lbMP1);
+            this.guiaPrincipal.Controls.Add(this.lbUTG1);
+            this.guiaPrincipal.Controls.Add(this.lbUTG);
+            this.guiaPrincipal.Controls.Add(this.lbBB);
+            this.guiaPrincipal.Controls.Add(this.lbSB);
+            this.guiaPrincipal.Controls.Add(this.lbBTN);
+            this.guiaPrincipal.Location = new System.Drawing.Point(4, 22);
+            this.guiaPrincipal.Name = "guiaPrincipal";
+            this.guiaPrincipal.Padding = new System.Windows.Forms.Padding(3);
+            this.guiaPrincipal.Size = new System.Drawing.Size(808, 460);
+            this.guiaPrincipal.TabIndex = 1;
+            this.guiaPrincipal.Text = "tabPage2";
+            this.guiaPrincipal.UseVisualStyleBackColor = true;
+            // 
+            // lbBTN
+            // 
+            this.lbBTN.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lbBTN.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBTN.Location = new System.Drawing.Point(368, 125);
+            this.lbBTN.Name = "lbBTN";
+            this.lbBTN.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbBTN.Size = new System.Drawing.Size(76, 25);
+            this.lbBTN.TabIndex = 0;
+            this.lbBTN.Text = "BTN";
+            this.lbBTN.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbSB
+            // 
+            this.lbSB.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lbSB.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSB.Location = new System.Drawing.Point(519, 125);
+            this.lbSB.Name = "lbSB";
+            this.lbSB.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbSB.Size = new System.Drawing.Size(76, 25);
+            this.lbSB.TabIndex = 1;
+            this.lbSB.Text = "SB";
+            this.lbSB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbBB
+            // 
+            this.lbBB.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lbBB.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBB.Location = new System.Drawing.Point(643, 243);
+            this.lbBB.Name = "lbBB";
+            this.lbBB.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbBB.Size = new System.Drawing.Size(76, 25);
+            this.lbBB.TabIndex = 2;
+            this.lbBB.Text = "BB";
+            this.lbBB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbUTG
+            // 
+            this.lbUTG.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lbUTG.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUTG.Location = new System.Drawing.Point(564, 368);
+            this.lbUTG.Name = "lbUTG";
+            this.lbUTG.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbUTG.Size = new System.Drawing.Size(76, 25);
+            this.lbUTG.TabIndex = 3;
+            this.lbUTG.Text = "UTG";
+            this.lbUTG.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbUTG1
+            // 
+            this.lbUTG1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lbUTG1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUTG1.Location = new System.Drawing.Point(434, 376);
+            this.lbUTG1.Name = "lbUTG1";
+            this.lbUTG1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbUTG1.Size = new System.Drawing.Size(76, 25);
+            this.lbUTG1.TabIndex = 4;
+            this.lbUTG1.Text = "UTG + 1";
+            this.lbUTG1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbMP1
+            // 
+            this.lbMP1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lbMP1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMP1.Location = new System.Drawing.Point(306, 375);
+            this.lbMP1.Name = "lbMP1";
+            this.lbMP1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbMP1.Size = new System.Drawing.Size(76, 25);
+            this.lbMP1.TabIndex = 5;
+            this.lbMP1.Text = "MP1";
+            this.lbMP1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbMP2
+            // 
+            this.lbMP2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lbMP2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMP2.Location = new System.Drawing.Point(173, 367);
+            this.lbMP2.Name = "lbMP2";
+            this.lbMP2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbMP2.Size = new System.Drawing.Size(76, 25);
+            this.lbMP2.TabIndex = 6;
+            this.lbMP2.Text = "MP2";
+            this.lbMP2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbHJ
+            // 
+            this.lbHJ.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lbHJ.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHJ.Location = new System.Drawing.Point(92, 244);
+            this.lbHJ.Name = "lbHJ";
+            this.lbHJ.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbHJ.Size = new System.Drawing.Size(76, 25);
+            this.lbHJ.TabIndex = 7;
+            this.lbHJ.Text = "HJ";
+            this.lbHJ.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbCO
+            // 
+            this.lbCO.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lbCO.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCO.Location = new System.Drawing.Point(215, 125);
+            this.lbCO.Name = "lbCO";
+            this.lbCO.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbCO.Size = new System.Drawing.Size(76, 25);
+            this.lbCO.TabIndex = 8;
+            this.lbCO.Text = "CO";
+            this.lbCO.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // HandRanges
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Painel);
+            this.Name = "HandRanges";
+            this.Text = "Hand Ranges";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Painel.ResumeLayout(false);
+            this.guiaPrincipal.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl Painel;
+        private System.Windows.Forms.TabPage guiaPrincipal;
+        private System.Windows.Forms.Label lbSB;
+        private System.Windows.Forms.Label lbBTN;
+        private System.Windows.Forms.Label lbHJ;
+        private System.Windows.Forms.Label lbMP2;
+        private System.Windows.Forms.Label lbMP1;
+        private System.Windows.Forms.Label lbUTG1;
+        private System.Windows.Forms.Label lbUTG;
+        private System.Windows.Forms.Label lbBB;
+        private System.Windows.Forms.Label lbCO;
+    }
+}
+
